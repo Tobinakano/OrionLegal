2fb29990e10b78b2c50a76bd0bdec2c979d9b77b
Tobinakano <nicolashdezrdo@gmail.com>
2025-10-20 18:10:33 -0500

    Configuración de archivos estáticos con WhiteNoise

--- OrionLegal/settings.py
+++ OrionLegal/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,4 +126,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp', 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
